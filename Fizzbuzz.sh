d05be5e8715272aa237d332a03d89a465124e6e5
Hndl.CGB <clive.bury@gmail.com>
2018-11-19 13:34:28 +0000

    feeback

--- Fizzbuzz.sh
+++ Fizzbuzz.sh
@@ -30,3 +30,24 @@ do
 numtoprint="$(ismultiple $num)"
 echo "$numtoprint"
 done
+
+
+# Feedback by CGB
+# CGB : Line 03: The naming of a function is a key activity, get the name write and its explains what it does.
+# CGB : Line 04: Function name 'ismultiple' - are we checking if $1 is a multiple of 15||5||3, 
+#       feels like a 'is 1 multiple of 15'? or is 1 divisable by 15?
+# CGB : Line 04: Regardless of the function, the result looks spot on.
+# CGB : Line 08: Well spotted on the output of both Fizz and Buzz, you have the output as per spec - Fizzbuzz.
+# CGB : Line 23: its good to set constants, helps future changes easier to make. Do we need to be able to pick the
+#       constants out from other variables? 
+# CGB : Line 27: perhaps the comment is not required.  After all, we can see it's a loop, thoughts?
+# CGB : Line 30: numtoprint - is it always a number? or is it sometimes Fizz || Buzz or Fizzbuzz.
+# CGB : Line 30: And line 31 - indentation.
+# CGB : Line 09: And line 13, 17 - do we need to exit 3 times in 3 different? there is no exit after the last else :) 
+# CGB : Closing Statement:
+#       - It's good you didnt to drawn into what the future changes could be.  I think sometimes, we can worry too
+#       much about what possible changes could come in the future and over complicate the solution. 
+#       - good attention to the solution as it's based on the spec.
+#       - choose variable, constants, functions names with care. Get it right and the code documents itself.  
+#       this is difficult to do, easier to say..
+#       - Overall: good practical solution.
